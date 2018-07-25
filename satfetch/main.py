96035991afbf35f26c35be20ddf4a62d07198763
Matthew Hanson <matt.a.hanson@gmail.com>
2018-07-24 20:12:00 -0400

    use intermediate temp VRT file

--- satfetch/main.py
+++ satfetch/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
 import json
@@ -42,11 +42,6 @@ def main(scenes=None, review=False, print_md=None, print_cal=False,
     else:
         scenes = Scenes.load(scenes)
 
-    if review:
-        if not os.getenv('IMGCAT', None):
-            raise ValueError('Set IMGCAT envvar to terminal image display program to use review feature')
-        scenes.review_thumbnails()
-
     # print metadata
     if print_md is not None:
         scenes.print_scenes(print_md)
@@ -72,13 +67,13 @@ def main(scenes=None, review=False, print_md=None, print_cal=False,
         aoiname = f.name
         aoistr = json.dumps(feature)
         f.write(aoistr)
-
+    from pdb import set_trace
     geovec = gippy.GeoVector(aoiname)
 
     #opts = {'COMPRESS': 'DEFLATE', 'PREDICTOR': '2', 'TILED': 'YES', 'BLOCKXSIZE': '512', 'BLOCKYSIZE': '512'}
     #opts = {'COMPRESS': 'LZW', 'TILED': 'YES', 'BLOCKXSIZE': '512', 'BLOCKYSIZE': '512'}
     opts = {'TILED': 'YES', 'BLOCKXSIZE': '512', 'BLOCKYSIZE': '512'}
-    for date in scenes.dates()[0:1]:
+    for date in scenes.dates():
         _scenes = [s for s in scenes if s.date == date]
         # TODO - seperate out by platform
 
@@ -86,30 +81,32 @@ def main(scenes=None, review=False, print_md=None, print_cal=False,
         fname = _scenes[0].get_filename()
         # TODO - do not hardcode tif 
         fout = os.path.join(path, fname + '.tif')
-       
+        gippy.Options.set_verbose(5)
         if not os.path.exists(fout):
             try:
-                geoimgs = []
-                for s in _scenes:
-                    if len(download) == 0:
-                        download = s.name_to_band.keys()
-                    assets = [s.asset(k) for k in download]
-                    filenames = [a['href'].replace('https:/', '/vsicurl') for a in assets]
-                    geoimg = gippy.GeoImage.open(filenames)
-                    geoimg.set_nodata(0)
-                    imgout = gippy.GeoImage.create_from(geoimg, 'test.vrt', nb=0, format='VRT')
-                    imgout.add_band(geoimg[0])
+                with tempfile.TemporaryDirectory() as outdir:
+                    geoimgs = []
+                    for s in _scenes:
+                        geoimgs.append(make_vrt(s, download, outdir=outdir))
 
-                    #imgout = geoimg.save(format='VRT')
-                    #set_trace()
-                    #geoimgs.append(imgout)
-
-                # default to first image res and srs
-                #res = geoimgs[0].resolution()
-                #imgout = algs.cookie_cutter(geoimgs, fout, geovec[0], xres=res.x(), yres=res.y(), proj=geoimgs[0].srs(), options=opts)
-                #print(imgout.info())
+                    # default to first image res and srs
+                    res = geoimgs[0].resolution()
+                    imgout = algs.cookie_cutter(geoimgs, fout, geovec[0], xres=res.x(), yres=res.y(), proj=geoimgs[0].srs(), options=opts)
+                    print(imgout.info())
             except Exception as err:
                 print('Error: ', str(err))
+
+
+def make_vrt(scene, keys=None, outdir='./'):
+    """ Build a VRT from these assets """
+    if keys is None:
+        keys = scene.name_to_band.keys()
+    assets = [scene.asset(k) for k in keys]
+    fout = os.path.join(outdir, os.path.abspath(scene.id + '.vrt'))
+    filenames = [a['href'] for a in assets]
+    gdal.BuildVRT(fout, filenames, separate=True, srcNodata=0)
+    geoimg = gippy.GeoImage(fout)
+    return geoimg
 
 
 def cli():
