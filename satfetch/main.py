89b4040e4b8dff8d932d3eeae4ee14a36104c624
Matthew Hanson <matt.a.hanson@gmail.com>
2018-07-31 17:08:13 -0400

    updated main

--- satfetch/main.py
+++ satfetch/main.py
@@ -7,19 +7,18 @@ import os
 import sys
 import tempfile
 from .version import __version__
-
-from osgeo import gdal
 import gippy
-import gippy.algorithms as algs
+from osgeo import gdal
+
 import satsearch.config as config
-from satsearch import Search, Scenes
+from satsearch import Search, Scene, Scenes
 from satsearch.main import main as satsearch
 from satsearch.parser import SatUtilsParser
-from satsearch.scene import Scenes
+from satfetch import fetch
+
 
 logger = logging.getLogger(__name__)
 
-from pdb import set_trace
 
 class SatFetchParser(SatUtilsParser):
 
@@ -33,34 +32,15 @@ class SatFetchParser(SatUtilsParser):
         self.download_group.add_argument('--separate', help='Do not combine bands into files', action='store_true', default=False)
 
 
-def derived_stac_item(items):
-    """ Create a derived STAC item from these """
-    # data provenance, iterate through links
-    links = {'parent-%s' % i: i['link']['self'] for i, item in enumerate(items)}
-    # calculate composite geometry and bbox
-    # properties
-    props = items[0]['properties']
-    props = {
-        'id': '--'.join([i['properties'] for i in items]),
-        'datetime': items[0]['properties']['datetime'],
-    }
-    item = {
-        'properties': props,
-        'bbox': bbox,
-        'geometry': geom,
-        'links': links,
-        'assets': {}
-    }
-    return item
-
-
-def main(scenes=None, download=None, **kwargs):
+def main(scenes=None, save=None, download=None, **kwargs):
     """ Main function for performing a search """
     scenes = satsearch(scenes, download=None, **kwargs)
 
     # if not downloading nothing more to do
     if download is None:
         return
+
+    import pdb; pdb.set_trace()
 
     # check that there is a valid geometry for clipping
     feature = scenes.properties.get('intersects', {})
@@ -72,55 +52,21 @@ def main(scenes=None, download=None, **kwargs):
         f.write(aoistr)
     geovec = gippy.GeoVector(aoiname)
 
-    opts = {'COMPRESS': 'DEFLATE', 'PREDICTOR': '2', 'INTERLEAVE': 'BAND',
-            'TILED': 'YES', 'BLOCKXSIZE': '512', 'BLOCKYSIZE': '512'}
+    derived_scenes = []
+    # for each date, combine scenes
     for date in scenes.dates():
         _scenes = [s for s in scenes if s.date == date]
-        # TODO - seperate out by platform
+        # TODO - split out by user specified metadata (e.g., platform, collection)
+        scene = fetch(_scenes, download, geovec[0])
+        derived_scenes.append(scene)
 
-        path = _scenes[0].get_path()
-        fname = _scenes[0].get_filename()
-        # TODO - do not hardcode tif 
-        fout = os.path.join(path, fname + '.tif')
-        if not os.path.exists(fout):
-            try:
-                with tempfile.TemporaryDirectory() as outdir:
-                    geoimgs = []
-                    for s in _scenes:
-                        geoimgs.append(open_image(s, download))
-
-                    # default to first image res and srs
-                    res = geoimgs[0].resolution()
-                    imgout = algs.cookie_cutter(geoimgs, fout, geovec[0], xres=res.x(), yres=res.y(), proj=geoimgs[0].srs(), options=opts)
-                    print(imgout.info())
-            except Exception as err:
-                print('Error: ', str(err))
-
-
-
-
-
-def fetch():
-    """ This fetches data from just the AOI and clips it """
-    pass
-
-
-
-def open_image(scene, keys=None, download=False):
-    """ Open these asset keys from scene as a gippy GeoImage """
-    if keys is None:
-        keys = scene.name_to_band.keys()
-    logger.debug('Opening scene %s (%s)' % (scene.id, ','.join(keys)))
-    assets = [scene.asset(k) for k in keys]
-    filenames = [a['href'].replace('https:/', '/vsicurl/https:/') for a in assets]
-    geoimg = gippy.GeoImage.open(filenames, update=False)
-    geoimg.set_nodata(0)
-    if download:
-        # download scenes first
-        fnames = scene.download(keys)
-        return gippy.GeoImage.open(fnames)
-    else:
-        return geoimg
+    props = {
+        'software': 'sat-fetch v%s' % __version__
+    }
+    derived_scenes = Scenes(derived_scenes, props)
+    if save is not None:
+        derived_scenes.save(save)
+    return derived_scenes
 
 
 def cli():
